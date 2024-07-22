0da46bb8bc055e91bf036beee51cef0d81e3c42a
AnRuzhina <nastya.ruzhina0530@gmail.com>
2024-07-22 16:15:14 +0200

    First commit

---
+++ Hillel_HM02.py
@@ -0,0 +1,56 @@
+# from sys import maxsize
+# неизменные типы данных( int, non, bool,str)
+# var_1 = 'HI ALL'
+# print(var_1, id(var_1))
+# var_1 = ''
+# print(var_1, id(var_1))
+# print(maxsize)
+# max_int = 9223372036854775807 + 10
+# print(max_int, type(max_int), id(max_int))
+# non = None
+# print(1 if var_1 else 0)
+
+# изменные типи данных(
+# list_1 = [ 1, 2, 3, 'STR', None, True, False]
+
+# print(list_1)
+# print(id(list_1))
+# list_1.append(10)
+# print(id(list_1))
+# print(list_1)
+
+# user_data = int(input('Enter number:'))
+# multiplier = int(input('Enter multiplier:'))
+# print(user_data * multiplier)
+# print(-12 <  12)
+
+# var_1 = 10
+# var_2 = 1000
+# var_1 += var_2
+# print(var_1)
+# print(globals())
+
+# Hillel_HM02
+# operator.lt
+a = 5
+b = - 13
+print(a < b)
+#operator.le
+print(a <= b)
+#  operator.eq
+print(a == b)
+#operator.ne
+print(a != b)
+#operator.ge
+print(a >= b)
+#operator.gt
+print(a > b)
+# ** - степень
+print(2 ** 5)
+# float
+print(1.04 + 2)
+
+
+
+
+
