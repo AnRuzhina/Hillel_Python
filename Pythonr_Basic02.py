4f611bc9a269c23680ba3c87f3bcc9e98b62fc58
AnRuzhina <nastya.ruzhina0530@gmail.com>
2024-07-22 16:57:46 +0200

    First commit

---
+++ Pythonr_Basic02.py
@@ -0,0 +1,14 @@
+# Необхідно "перевернути" 5-ти значне число
+
+print("Enter your number")
+
+number = input()[:: -1]
+print(number)
+
+# Виведення числа в стовпчик
+
+print("Enter your number")
+
+number = input()
+print(*number, sep='\n')
+
