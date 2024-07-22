bab8281390b0ad0a58e944e60978de6d2ac058a2
AnRuzhina <nastya.ruzhina0530@gmail.com>
2024-07-22 18:06:15 +0200

    First commit

--- Python_Basic03.py
+++ Python_Basic03.py
@@ -4,3 +4,21 @@ print("Enter your number")
 
 number = input()
 print(*number, sep='\n')
+
+# or
+
+print("Enter your number")
+
+number = int(input())
+
+number1 = number // 1000
+print(number1)
+
+number2 = number % 1000 // 100
+print(number2)
+
+number3 = number % 100 // 10
+print(number3)
+
+number4 = number % 10 // 1
+print(number4)
