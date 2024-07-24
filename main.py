9608e81c54e7a965b58a52347d7abf506508ff8d
AnRuzhina <nastya.ruzhina0530@gmail.com>
2024-07-24 20:27:46 +0200

    First commit

--- main.py
+++
@@ -1,8 +0,0 @@
-
-# print('Hi!!')
-# print('1', '2', 2 ,None, True, sep='|', end='\n\n')
-# print('1', '2', 2 ,None, True, sep='|')
-# text = str(10000)
-# print(type(10000)
-# print(type(text))
-# print(int(text) + 10)
