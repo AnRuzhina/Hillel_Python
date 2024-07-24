7103778782aedf4804600f28ac372a2e22ea8524
AnRuzhina <nastya.ruzhina0530@gmail.com>
2024-07-24 21:59:12 +0200

    First commit

--- Python_Basic05.py
+++ Python_Basic05.py
@@ -1,6 +1,7 @@
 # Перемістити елемент у списку
+# Я для вашої зручності продублювала умову для кожного рішення, щоб результат одразу по всім було видно:)
 
-list1 = []
+list1 = [12, 3, 4, 10]
 if len(list1) / 2 != 0 and len(list1) != 1 and len(list1) != 3 and len(list1) != 5 and len(list1) / 4 != 0:
     list1.remove(10)
     list1.insert(0, 10)
@@ -16,3 +17,55 @@ if len(list1) / 2 != 0 and len(list1) != 1 and len(list1) == 5 and not len(list1
     list1.remove(8)
     list1.insert(0, 8)
     print(list1)
+
+list2 = [1]
+if len(list2) / 2 != 0 and len(list2) != 1 and len(list2) != 3 and len(lis21) != 5 and len(list2) / 4 != 0:
+    list2.remove(10)
+    list2.insert(0, 10)
+    print(list2)
+
+if len(list2) / 2 != 0 and len(list2) == 1 and len(list2) != 3 and len(list2) != 5:
+    print(list2)
+
+if len(list2) / 2 == 0 and len(list2) != 1 and len(list2) != 3 and len(list2) != 5:
+    print(list2)
+
+if len(list2) / 2 != 0 and len(list2) != 1 and len(list2) == 5 and not len(list2) / 4 == 0:
+    list2.remove(8)
+    list2.insert(0, 8)
+    print(list2)
+
+list3 = []
+if len(list3) / 2 != 0 and len(list3) != 1 and len(list3) != 3 and len(list3) != 5 and len(list3) / 4 != 0:
+    list3.remove(10)
+    list3.insert(0, 10)
+    print(list3)
+
+if len(list3) / 2 != 0 and len(list3) == 1 and len(list3) != 3 and len(list3) != 5:
+    print(list3)
+
+if len(list3) / 2 == 0 and len(list3) != 1 and len(list3) != 3 and len(list3) != 5:
+    print(list3)
+
+if len(list3) / 2 != 0 and len(list3) != 1 and len(list3) == 5 and not len(list3) / 4 == 0:
+    list3.remove(8)
+    list3.insert(0, 8)
+    print(list3)
+
+list4 = [12, 3, 4, 10, 8]
+
+if len(list4) / 2 != 0 and len(list4) != 1 and len(list4) != 3 and len(list4) != 5 and len(list4) / 4 != 0:
+    list4.remove(10)
+    list4.insert(0, 10)
+    print(list4)
+
+if len(list4) / 2 != 0 and len(list4) == 1 and len(list4) != 3 and len(list4) != 5:
+    print(list4)
+
+if len(list4) / 2 == 0 and len(list4) != 1 and len(list4) != 3 and len(list4) != 5:
+    print(list4)
+
+if len(list4) / 2 != 0 and len(list4) != 1 and len(list4) == 5 and not len(list4) / 4 == 0:
+    list4.remove(8)
+    list4.insert(0, 8)
+    print(list4)
