f3c190dc780f21a868df1fc4301ad38816a761d2
AnRuzhina <nastya.ruzhina0530@gmail.com>
2024-08-01 20:49:03 +0200

    First commit

--- Python_Basic07.py
+++ Python_Basic07.py
@@ -1,6 +1,10 @@
-list1 = [1, 0, 3, 0, 4]
-list1 = [i for i in list1 if i != 0]
-zeros = len(list1)
-list1.extend([0] * zeros)
-list1.remove(0)
-print(list1)
+# Перемістити всі нулі до кінця списку
+
+list1 = [0, 0, 2, 1, 3, 0, 0, 3]
+if list1 != [0]:
+    list1 = [i for i in list1 if i != 0]
+    list1.extend([0] * len(list1))
+    print(list1)
+else:
+    list1 = [0]
+    print(list1)
