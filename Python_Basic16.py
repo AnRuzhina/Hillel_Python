efdfe34230a16a7d2d2dc476f288efd0e07420df
AnRuzhina <nastya.ruzhina0530@gmail.com>
2024-08-17 14:01:48 +0200

    First commit

--- Python_Basic16.py
+++ Python_Basic16.py
@@ -12,4 +12,4 @@ print(result)
 
 assert say_hi("Alex", 32) == "Hi. My name is Alex and I'm 32 years old", 'Test1'
 assert say_hi("Frank", 68) == "Hi. My name is Frank and I'm 68 years old", 'Test2'
-print('ОК')
+
