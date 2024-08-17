fe84a73f8edf78ce3e757722bf30ccaaef589601
AnRuzhina <nastya.ruzhina0530@gmail.com>
2024-08-17 16:17:38 +0200

    First commit

---
+++ Python_Basic17.py
@@ -0,0 +1,19 @@
+# Модифікувати рядок
+def correct_sentence(text):
+    corrected_text = text[0].upper() + text[1:]
+#    corrected_text = text.capitalize()
+    if not corrected_text.endswith('.'):
+        corrected_text += '.'
+
+    return corrected_text
+
+
+result = correct_sentence(text=input(""))
+print(result)
+
+assert correct_sentence("greetings, friends") == "Greetings, friends.", 'Test1'
+assert correct_sentence("hello") == "Hello.", 'Test2'
+assert correct_sentence("Greetings. Friends") == "Greetings. Friends.", 'Test3'
+assert correct_sentence("Greetings, friends.") == "Greetings, friends.", 'Test4'
+assert correct_sentence("greetings, friends.") == "Greetings, friends.", 'Test5'
+print('ОК')
