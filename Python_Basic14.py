2531feee62ccd72332d3195b7c49387277f1c4c9
AnRuzhina <nastya.ruzhina0530@gmail.com>
2024-08-18 14:32:20 +0200

    First commit

--- Python_Basic14.py
+++ Python_Basic14.py
@@ -10,13 +10,13 @@ seconds = user_number % 60
 # Визначення правильної форми слова "день"
 d = days % 10
 if 11 <= days % 100 <= 14:
-    day_word = "днів"
+    day_word = "дней"
 elif d == 1:
     day_word = "день"
 elif 2 <= d <= 4:
-    day_word = "дні"
+    day_word = "дня"
 else:
-    day_word = "днів"
+    day_word = "дней"
 
 # Формування та виведення результату
 print(f'{days} {day_word}, {hours:02}:{minutes:02}:{seconds:02}')
