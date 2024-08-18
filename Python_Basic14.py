3dd710fe20f8691a87c645976e7627b596443321
AnRuzhina <nastya.ruzhina0530@gmail.com>
2024-08-18 14:31:19 +0200

    First commit

--- Python_Basic14.py
+++ Python_Basic14.py
@@ -6,11 +6,17 @@ days = user_number // 86400
 hours = user_number // 3600 % 24
 minutes = user_number // 60 % 60
 seconds = user_number % 60
+
+# Визначення правильної форми слова "день"
 d = days % 10
-if 0 <= d <= 100:
-    if d == 1:
-        print(f'{days}', 'день' ',', f'{hours:02}:', f'{minutes:02}:', f'{seconds:02}')
-    elif 2 <= d <= 4:
-        print(f'{days}', 'дня', ',', f'{hours:02}:', f'{minutes:02}:', f'{seconds:02}')
-    else:
-        print(f'{days}', 'дней', ',', f'{hours:02}:', f'{minutes:02}:', f'{seconds:02}')
+if 11 <= days % 100 <= 14:
+    day_word = "днів"
+elif d == 1:
+    day_word = "день"
+elif 2 <= d <= 4:
+    day_word = "дні"
+else:
+    day_word = "днів"
+
+# Формування та виведення результату
+print(f'{days} {day_word}, {hours:02}:{minutes:02}:{seconds:02}')
